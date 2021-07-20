79f4b4a90229d34fe2181c01993658c0c901aaee
raoul2000 <manu34@free.fr>
2021-07-21 00:42:42 +0200

    feature: render QR code in user settings page

--- server/views/user-settings/index.php
+++ server/views/user-settings/index.php
@@ -15,6 +15,18 @@ $this->title = 'User Settings';
             <div class="col-sm-2">Email</div>
             <div class="col-sm-10"><?= $userModel->email ?></div>
         </div>
+        <div class="row">
+            <div class="col-sm-2">API Key</div>
+            <div class="col-sm-10">
+                
+                <?php if(!empty($qrCode)): ?>
+                    <?= $userModel->api_key ?><br/>
+                    <img src="<?=  $qrCode->writeDataUri()?>" title="api key : QR code" alt="api key qr code"/>
+                <?php else: ?>
+                    <em>no set</em>
+                <?php endif ?>
+            </div>
+        </div>
     </div>
 </div>
 
