3da36943401938e75e53964985265f4157d5de40
raoul2000 <manu34@free.fr>
2021-07-24 12:42:19 +0200

    add extra attr to book model

--- server/models/Book.php
+++ server/models/Book.php
@@ -61,4 +61,50 @@ class Book extends \yii\db\ActiveRecord
             'updated_at' => 'Updated At',
         ];
     }
+
+    public function getPings()
+    {
+        return $this->hasMany(BookPing::class, ['book_id' => 'id']);
+    }
+
+    public function getPingsCount()
+    {
+        //TODO: requires extra query - could be optimized (or not used)
+        if ($this->isNewRecord) {
+            return null; // this avoid calling a query searching for null primary keys
+        }
+
+        return empty($this->pingsAggregation) ? 0 : $this->pingsAggregation[0]['counted'];
+    }
+
+    /**
+     * Declares new relation based on 'orders', which provides aggregation.
+     */
+    public function getPingsAggregation()
+    {
+        return $this->getPings()
+            ->select(['book_id', 'counted' => 'count(*)'])
+            ->groupBy('book_id')
+            ->asArray(true);
+    }
+
+    public function getReviews()
+    {
+        return $this->hasMany(BookReview::class, ['book_id' => 'id']);
+    }
+    public function getReviewsCount()
+    {
+        if ($this->isNewRecord) {
+            return null; // this avoid calling a query searching for null primary keys
+        }
+
+        return empty($this->reviewsAggregation) ? 0 : $this->reviewsAggregation[0]['counted'];
+    }
+    public function getReviewsAggregation()
+    {
+        return $this->getReviews()
+            ->select(['book_id', 'counted' => 'count(*)'])
+            ->groupBy('book_id')
+            ->asArray(true);
+    }
 }
