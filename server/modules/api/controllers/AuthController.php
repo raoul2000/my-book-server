90c24e973590a1f0aae5ff475bbd13ad6d47c069
raoul2000 <manu34@free.fr>
2021-07-26 23:50:35 +0200

    feature: add auth/login endpoint

---
+++ server/modules/api/controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\modules\api\controllers;
+
+use Yii;
+
+use app\models\User;
+use yii\web\BadRequestHttpException;
+use yii\web\UnauthorizedHttpException;
+
+class AuthController extends BaseApiController
+{
+    public $modelClass = 'app\models\Book';
+
+    protected function verbs()
+    {
+        return [
+            'login' => ['POST', 'OPTIONS'],
+        ];
+    }
+    /**
+     * remove authentication filter
+     */
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        unset($behaviors['authenticator']);
+        return $behaviors;
+    }    
+
+    public function actionLogin()
+    {
+        $params = Yii::$app->request->post();
+        if (empty($params['username']) || empty($params['password'])) {
+            throw new BadRequestHttpException('username and password are required');
+        }
+
+        $user = User::findByUsername($params['username']);
+        if(empty($user)) {
+            throw new UnauthorizedHttpException('invalid credentials');
+        }
+        
+        if ($user->validatePassword($params['password'])) {
+            Yii::$app->response->setStatusCode(200);
+            return [
+                'success' => true,
+                'api_key' => $user->api_key
+            ];
+        } else {
+            throw new UnauthorizedHttpException('invalid credentials');
+        }
+    }
+}
