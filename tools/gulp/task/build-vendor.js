6d0255c16ad8d198f7efb31275832e4c8600e088
raoul2000 <manu34@free.fr>
2021-07-31 12:41:05 +0200

    refactor

---
+++ tools/gulp/task/build-vendor.js
@@ -0,0 +1,60 @@
+const { series, src, dest } = require('gulp');
+const exec = require('child_process').exec;
+const del = require('del');
+const zip = require('gulp-zip');
+const path = require('path');
+
+function cleanComposer() {
+    return del('../../build/composer/**', { force: true });
+}
+
+function copyComposer() {
+    return src([
+        '../../src/composer.lock',
+        '../../src/composer.json'   
+    ], { base: '../../src/' })
+        .pipe(dest('../../build/composer'));
+}
+
+function composerInstall() {
+    return new Promise((resolve, reject) => {
+        const composer = exec('php ../../tools/composer install --no-dev --prefer-dist',
+            {
+                cwd: '../../build/composer'
+            });
+        composer.stdout.on('data', (data) => {
+            console.log(data.toString().replace(/(\n|\r)+$/, ''));
+        });
+        // note that composer output messages to stderr
+        // see https://github.com/composer/composer/issues/3795
+        composer.stderr.on('data', (data) => {
+            console.log(data.toString().replace(/(\n|\r)+$/, ''));
+        });
+        composer.stdout.on('data', (data) => {
+            console.log(data.toString().replace(/(\n|\r)+$/, ''));
+        });
+
+        composer.on('exit', (code) => {
+            if (code == 0) {
+                resolve(true);
+            } else {
+                reject(`composer install failed - exit code = ${code}`);
+            }
+        });
+    });
+}
+
+function zipVendor() {
+    return src([
+        '../../build/composer/vendor/**'
+    ])
+    .pipe(zip('vendor.zip'))
+    .pipe(dest('../../build/zip'));
+}
+
+
+exports.copyComposer = copyComposer;
+exports.composerInstall = composerInstall;
+exports.cleanComposer = cleanComposer;
+exports.zipVendor = zipVendor;
+exports.buildVendor = series(cleanComposer, copyComposer, composerInstall);
