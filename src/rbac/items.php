b8692909c09238d7105754f4bab5e50052117727
raoul2000 <manu34@free.fr>
2021-08-20 12:47:14 +0200

    add rbac permission 'administrate'

--- src/rbac/items.php
+++ src/rbac/items.php
@@ -1,7 +1,14 @@
 <?php
 
 return [
+    'administrate' => [
+        'type' => 2,
+        'description' => 'Administrate the system',
+    ],
     'admin' => [
         'type' => 1,
+        'children' => [
+            'administrate',
+        ],
     ],
 ];
