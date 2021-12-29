3f1547e53b689379926bf4bac46804adb27ebf43
raoul2000 <manu34@free.fr>
2021-12-30 00:27:46 +0100

    update db schema name (dev)

--- src/config/db.php
+++ src/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=my_data',
+    'dsn' => 'mysql:host=localhost;dbname=my_books',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
